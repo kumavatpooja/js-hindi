2fc6de550aa18eef5a8cca9dbb27e8938a877efb
kumavatpooja <88366585+kumavatpooja@users.noreply.github.com>
2023-11-28 09:05:53 +0000

    datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,19 @@
+"use strick";
+let age = 18
+let name = "pooja"
+console.log(typeof "pooja");
+console.log(typeof age);
+console.log(typeof null);
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
