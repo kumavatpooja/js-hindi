aa62c207f8fe55d2b66737a134af7066fae72be6
kumavatpooja <88366585+kumavatpooja@users.noreply.github.com>
2023-11-29 13:25:49 +0000

    iifi

---
+++ 3_basics/4_Iifi.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('pooja')
+
+
